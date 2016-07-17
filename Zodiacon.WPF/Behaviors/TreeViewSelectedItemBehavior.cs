74f4518de63fbc019b3e28a7bd172c34c90fc35f
Pavel Yosifovich <zodiacon@live.com>
2016-07-17 11:28:11 +0300

    Added tree view behavior

---
+++ Zodiacon.WPF/Behaviors/TreeViewSelectedItemBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Zodiacon.WPF.Behaviors {
+    public class TreeViewSelectedItemBehavior : Behavior<TreeView> {
+        protected override void OnAttached() {
+            base.OnAttached();
+
+            AssociatedObject.SelectedItemChanged += AssociatedObject_SelectedItemChanged;
+        }
+
+        protected override void OnDetaching() {
+            AssociatedObject.SelectedItemChanged -= AssociatedObject_SelectedItemChanged;
+
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
+            SelectedItem = AssociatedObject.SelectedItem;
+        }
+
+        public object SelectedItem {
+            get { return (object)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(nameof(SelectedItem), typeof(object), typeof(TreeViewSelectedItemBehavior), new PropertyMetadata(null));
+
+
+    }
+}
