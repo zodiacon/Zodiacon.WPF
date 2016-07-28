764e5403c11e9e0e455c52cc97abfb9ef00bc6bd
Pavel Yosifovich <zodiacon@live.com>
2016-07-27 22:10:26 -0700

    added SelectorActiveBehavior

---
+++ Zodiacon.WPF/Behaviors/SelectorActiveBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interactivity;
+using Prism;
+
+namespace Zodiacon.WPF.Behaviors {
+    /// <summary>
+    /// Notifies of active item change in a selector
+    /// </summary>
+    public sealed class SelectorActiveBehavior : Behavior<Selector> {
+        protected override void OnAttached() {
+            base.OnAttached();
+
+            AssociatedObject.SelectionChanged += AssociatedObject_SelectionChanged;
+        }
+
+        protected override void OnDetaching() {
+            AssociatedObject.SelectionChanged -= AssociatedObject_SelectionChanged;
+
+            base.OnDetaching();
+        }
+
+        public object ActiveItem {
+            get { return (object)GetValue(ActiveItemProperty); }
+            set { SetValue(ActiveItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty ActiveItemProperty =
+            DependencyProperty.Register(nameof(ActiveItem), typeof(object), typeof(SelectorActiveBehavior), 
+                new PropertyMetadata(null, (s, e) => ((SelectorActiveBehavior)s).OnActiveItemChanged(e)));
+
+        private void OnActiveItemChanged(DependencyPropertyChangedEventArgs e) {
+            var oldItem = e.OldValue;
+            if(oldItem != null)
+                SetActive(oldItem, false);
+            var newItem = e.NewValue;
+            if(newItem != null)
+                SetActive(newItem, true);
+        }
+
+        private void AssociatedObject_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
+            if(e.RemovedItems.Count > 0) {
+                SetActive(e.RemovedItems[0], false);
+            }
+            if(e.AddedItems.Count > 0) {
+                SetActive(e.AddedItems[0], true);
+            }
+        }
+
+        private void SetActive(object item, bool active) {
+            var activeAware = item as IActiveAware;
+
+            if(activeAware != null) {
+                activeAware.IsActive = active;
+            }
+            if(active) {
+                ActiveItem = item;
+                AssociatedObject.SelectedItem = item;
+            }
+        }
+    }
+}
---
+++ Zodiacon.WPF/PropertyFollower.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using Prism.Mvvm;
+
+namespace Zodiacon.WPF {
+    /// <summary>
+    /// Propagates property change from one object to another
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TTarget"></typeparam>
+    public sealed class PropertyFollower<TSource, TTarget> : IDisposable
+        where TSource : INotifyPropertyChanged
+        where TTarget : BindableBase {  // ensures that OnPropertyChanged method exists
+        HashSet<string> _propertyNames = new HashSet<string>();
+        TSource _source;
+        dynamic _target;
+        Dictionary<string, Action<string>> _extraWork;
+
+        public PropertyFollower(TSource source, TTarget target, params string[] propertyNames) {
+            Debug.Assert(source != null && target != null);
+
+            if(source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if(target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            if(propertyNames == null)
+                throw new ArgumentException("At least one property must be provided", nameof(propertyNames));
+
+            source.PropertyChanged += source_PropertyChanged;
+            _source = source;
+            _target = target;
+            foreach(var name in propertyNames)
+                _propertyNames.Add(name);
+
+        }
+
+        void source_PropertyChanged(object sender, PropertyChangedEventArgs e) {
+            if(_propertyNames.Contains(e.PropertyName)) {
+                _target.OnPropertyChanged(e.PropertyName);  // dynamic is used because OnPropertyChanged is protected
+                Action<string> action;
+                if(_extraWork != null && _extraWork.TryGetValue(e.PropertyName, out action))
+                    action(e.PropertyName);
+            }
+        }
+
+        public PropertyFollower<TSource, TTarget> Add(string propertyName, Action<string> extraWork = null) {
+            _propertyNames.Add(propertyName);
+            if(extraWork != null) {
+                if(_extraWork == null)
+                    _extraWork = new Dictionary<string, Action<string>>();
+                _extraWork.Add(propertyName, extraWork);
+            }
+            return this;
+        }
+
+        public void Dispose() {
+            _source.PropertyChanged -= source_PropertyChanged;
+        }
+    }
+}
