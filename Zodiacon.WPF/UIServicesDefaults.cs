97a1dfd479befee6c7f0452ee94726143663e85b
Pavel Yosifovich <zodiacon@live.com>
2016-08-27 12:02:12 +0300

    exposed defaults without MEF

---
+++ Zodiacon.WPF/UIServicesDefaults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zodiacon.WPF {
+    public static class UIServicesDefaults {
+        public static readonly IDialogService DialogService = DefaultDialogService.Instance;
+        public static readonly IFileDialogService FileDialogService = SimpleFileDialogService.Instance;
+        public static readonly IMessageBoxService MessageBoxService = SimpleMessageBox.Instance;
+    }
+}
