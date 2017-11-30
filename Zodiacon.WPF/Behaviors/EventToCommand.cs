5d96d5c9d2007457f6bf0c2a599faca2145c3363
Pavel Yosifovich <zodiacon@live.com>
2017-11-30 13:32:15 +0200

    added EventToCommand behavior

---
+++ Zodiacon.WPF/Behaviors/EventToCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Zodiacon.WPF.Behaviors {
+    public sealed class EventToCommand : Behavior<DependencyObject> {
+        EventInfo _event;
+        Delegate _handler;
+
+        protected override void OnAttached() {
+            base.OnAttached();
+
+            if (EventName != null) {
+                _event = AssociatedObject.GetType().GetEvent(EventName);
+                if (_event != null) {
+                    _handler = Delegate.CreateDelegate(_event.EventHandlerType, this, GetType().GetMethod(nameof(OnEvent), 
+                        BindingFlags.NonPublic | BindingFlags.Instance));
+                    _event.AddEventHandler(AssociatedObject, _handler);
+                }
+            }
+        }
+
+        void OnEvent(object sender, EventArgs args) {
+            if (Command.CanExecute(args))
+                Command.Execute(args);
+        }
+
+        protected override void OnDetaching() {
+            if (_event != null) {
+                _event.RemoveEventHandler(AssociatedObject, _handler);
+            }
+
+            base.OnDetaching();
+        }
+
+        public ICommand Command {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(EventToCommand), new PropertyMetadata(null));
+
+
+        public string EventName {
+            get { return (string)GetValue(EventNameProperty); }
+            set { SetValue(EventNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty EventNameProperty =
+            DependencyProperty.Register(nameof(EventName), typeof(string), typeof(EventToCommand), new PropertyMetadata(null));
+
+
+    }
+}
--- Zodiacon.WPF/Behaviors/SelectedItemHelper.cs
+++ Zodiacon.WPF/Behaviors/SelectedItemHelper.cs
@@ -23,8 +23,12 @@ namespace Zodiacon.WPF.Behaviors {
 				new PropertyMetadata(false, OnBringIntoViewWhenSelected));
 
 		private static void OnBringIntoViewWhenSelected(DependencyObject d, DependencyPropertyChangedEventArgs e) {
-			if (!(bool)e.NewValue)
+			if (!(bool)e.NewValue) {
+				if (d is DataGrid dg) {
+					dg.SelectionChanged -= DataGridSelectionChanged;
+				}
 				return;
+			}
 
 			switch (d) {
 				case TreeViewItem item:
@@ -37,47 +41,29 @@ namespace Zodiacon.WPF.Behaviors {
 						item.BringIntoView();
 					break;
 
+				case DataGridRow item:
+					if (item.IsSelected)
+						item.BringIntoView();
+					break;
+
+				case DataGrid dg:
+					dg.SelectionChanged += DataGridSelectionChanged;
+					break;
+
 				case FrameworkElement item:
 					if (Selector.GetIsSelected(item))
 						item.BringIntoView();
 					break;
 
 			}
-
 		}
 
-		// for DataGrid specifically
-
-		public static bool GetEnsureSelectedVisible(DependencyObject obj) {
-			return (bool)obj.GetValue(EnsureSelectedVisibleProperty);
-		}
-
-		public static void SetEnsureSelectedVisible(DependencyObject obj, bool value) {
-			obj.SetValue(EnsureSelectedVisibleProperty, value);
-		}
-
-		public static readonly DependencyProperty EnsureSelectedVisibleProperty =
-			DependencyProperty.RegisterAttached("EnsureSelectedVisible", typeof(bool), typeof(SelectedItemHelper), 
-				new PropertyMetadata(false, OnEnsureSelectedVisible));
-
-		private static void OnEnsureSelectedVisible(DependencyObject d, DependencyPropertyChangedEventArgs e) {
-			var dg = d as DataGrid;
-			if (dg == null)
-				return;
-
-			if ((bool)e.NewValue)
-				dg.SelectionChanged += Dg_SelectionChanged;
-			else
-				dg.SelectionChanged -= Dg_SelectionChanged;
-		}
-
-		private static void Dg_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+		private static void DataGridSelectionChanged(object sender, SelectionChangedEventArgs e) {
 			var dg = sender as DataGrid;
 			Debug.Assert(dg != null);
 
 			if (dg.SelectedItem != null)
 				dg.ScrollIntoView(dg.SelectedItem);
 		}
-
 	}
 }
